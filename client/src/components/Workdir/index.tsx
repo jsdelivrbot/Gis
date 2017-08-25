b9ca6969ca1468c5c1d6b355f7e274d2d0557c85
Pramod Kumar <pramodkumar.damam73@gmail.com>
2017-08-25 21:23:44 +0530

    Add code prettify

--- client/src/components/Workdir/index.tsx
+++ client/src/components/Workdir/index.tsx
@@ -4,38 +4,46 @@ import {Panel} from "react-bootstrap";
 import {RootState} from "../../reducers";
 import {AppState} from "../../reducers/app";
 
-export namespace Workdir {
-	export interface Props {
-		diff?: Array<AppState.Diff>
-	}
+declare const PR: any;
 
-	export interface State {}
+export namespace Workdir {
+  export interface Props {
+    diff?: Array<AppState.Diff>
+  }
+
+  export interface State {}
 }
 
 @connect(mapStateToProps)
 class Workdir extends React.Component<Workdir.Props, Workdir.State> {
-	render() {
-		return (
-			<div>
-				<Panel>
-					{
-						this.props.diff.map(diff => (
-							<div>
-								<h4>{diff.path}</h4>
-								{diff.textHunks.map(h => <p>{h}</p>)}
-							</div>
-						))
-					}
-				</Panel>
-			</div>
-		)
-	}
+  componentDidMount() {
+    PR.prettyPrint()
+  }
+
+  render() {
+    return (
+      <div>
+      <Panel>
+      {
+        this.props.diff && this.props.diff.map((diff, index) => (
+          <div key={index}>
+          <h4>{diff.path}</h4>
+          {diff.textHunks && diff.textHunks.map((h, index) => (
+            <pre key={index} className="prettyprint">{h}</pre>
+            ))}
+          </div>
+          ))
+      }
+      </Panel>
+      </div>
+      )
+  }
 }
 
 function mapStateToProps(props: RootState) {
-	return {
-		diff: props.app.storage.diff
-	};
+  return {
+    diff: props.app.storage.diff
+  };
 }
 
 export default Workdir;

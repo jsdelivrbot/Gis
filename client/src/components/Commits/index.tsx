ad01a48f0f8485567dc13d26324ca243f71b0cc7
Pramod Kumar <pramodkumar.damam73@gmail.com>
2017-08-26 23:25:01 +0530

    Add link to commit id in Commits

--- client/src/components/Commits/index.tsx
+++ client/src/components/Commits/index.tsx
@@ -7,7 +7,8 @@ import "./styles.scss";
 
 export namespace Commits {
   export interface Props {
-    commits?: Array<AppState.Commit>
+    commits?: Array<AppState.Commit>,
+    config?: AppState.Config
   }
 
   export interface State {
@@ -29,6 +30,17 @@ class Commits extends React.Component<Commits.Props, Commits.State> {
     this.setState({limit: this.state.limit * 2});
   }
 
+  getCommitGitLink(id) {
+    const origin = this.props.config.origin;
+    if(origin) {
+      const tail = origin.substr(-4);
+      let host = origin;
+      if (tail === ".git") host = origin.substr(0, origin.length - 4);
+      return `${host}/commit/${id}`;
+    }
+    return "#";
+  }
+
   render() {
     const {limit} = this.state;
     return (
@@ -39,7 +51,9 @@ class Commits extends React.Component<Commits.Props, Commits.State> {
               {
                 this.props.commits.slice(0, limit).map((commit, index) => (
                   <ListGroupItem className="commit-item" key={index}>
-                    <div className="commit-id">{commit.id}</div>
+                    <div className="commit-id">
+                      <a href={this.getCommitGitLink(commit.id)} target="_blank">{commit.id}</a>
+                    </div>
                     <div>
                       {commit.author.name}&nbsp;
                       <span className="author-mail">({commit.author.email})</span>
@@ -67,7 +81,8 @@ class Commits extends React.Component<Commits.Props, Commits.State> {
 
 function mapStateToProps(state: RootState) {
   return {
-    commits: state.app.commits
+    commits: state.app.commits,
+    config: state.app.config
   };
 }
 

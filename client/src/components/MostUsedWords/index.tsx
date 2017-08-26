1b54348f90c6598b415f9cf770283ba0c0ea4a4c
Pramod Kumar <pramodkumar.damam73@gmail.com>
2017-08-27 00:45:35 +0530

    Add Pie chart for mostusedwords

--- client/src/components/MostUsedWords/index.tsx
+++ client/src/components/MostUsedWords/index.tsx
@@ -4,6 +4,8 @@ import {RootState} from "../../reducers";
 import {AppState} from "../../reducers/app";
 import {Panel} from "react-bootstrap";
 import Miner from "../../utils/miner";
+import {PieChart, Pie} from "recharts";
+import "./styles.scss";
 
 export namespace MostUsedWords {
   export interface Props {
@@ -16,15 +18,17 @@ export namespace MostUsedWords {
 @connect(mapStateToProps)
 class MostUsedWords extends React.Component<MostUsedWords.Props, MostUsedWords.State> {
   render() {
+    const words = this.props.commits && this.props.config 
+      ? Miner.getWordsUsedAlot(this.props.commits, this.props.config, 5)
+      : [];
     return(
-      <Panel>
-        {this.props.commits && this.props.config && 
-          Miner.getWordsUsedAlot(this.props.commits, this.props.config, 5).map((wordDic, index) => (
-            <div key={index}>
-              <strong>{wordDic.word}:&nbsp;</strong>{wordDic.count}
-            </div>
-          ))
-        }
+      <Panel className="most-used-words-container">
+        <PieChart width={150} height={150}>
+          <Pie 
+            data={words} valueKey="count" cx="50%" cy="50%" 
+            outerRadius={30} fill="#8884d8"
+            nameKey="word" label/>
+        </PieChart>
       </Panel>
     )
   }
